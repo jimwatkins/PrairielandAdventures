2878680a77c7e8f4c1194c7822b6b70ca0ff755c
Jim Watkins <jim@montana.local>
2015-11-09 16:16:17 -0600

    Added initial play service stubs.

--- app/src/main/java/com/bigskyway/skyler/prairielandadventures2/MultiplayerMenuActivity.java
+++ app/src/main/java/com/bigskyway/skyler/prairielandadventures2/MultiplayerMenuActivity.java
@@ -2,18 +2,41 @@ package com.bigskyway.skyler.prairielandadventures2;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.games.Player;
 
-public class MultiplayerMenuActivity extends Activity {
+
+public class MultiplayerMenuActivity extends FragmentActivity
+        implements EnableGooglePlayFragment.OnFragmentInteractionListener,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+
+    final String TAG = "MultiplayerMenuActivity";
+
+    // Client used to interact with Google APIs
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_multiplayer_main_screen);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API).addScope(Plus.SCOPE_PLUS_LOGIN)
+                .addApi(Games.API).addScope(Games.SCOPE_GAMES)
+                .build();
+
 
     }
 
@@ -39,6 +62,42 @@ public class MultiplayerMenuActivity extends Activity {
 //        startActivity(intent);
 //    }
 
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnected(): connected to Google APIs");
+        // Show sign-out button on main menu
+//        mMainMenuFragment.setShowSignInButton(false);
+
+        // Show "you are signed in" message on win screen, with no sign in button.
+//        mWinFragment.setShowSignInButton(false);
+
+        // Set the greeting appropriately on main menu
+        Player p = Games.Players.getCurrentPlayer(mGoogleApiClient);
+        String displayName;
+        if (p == null) {
+            Log.w(TAG, "mGamesClient.getCurrentPlayer() is NULL!");
+            displayName = "???";
+        } else {
+            displayName = p.getDisplayName();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
 
 
 }
