352213d7a43ab439c186689b2586e3691aa386e7
jimwatkins <jim@bigskyway.com>
2015-11-19 20:39:05 -0600

    Play might be working, more testing required.

--- app/src/main/java/com/bigskyway/skyler/prairielandadventures2/MultiplayerMenuActivity.java
+++ app/src/main/java/com/bigskyway/skyler/prairielandadventures2/MultiplayerMenuActivity.java
@@ -1,7 +1,11 @@
 package com.bigskyway.skyler.prairielandadventures2;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -12,6 +16,7 @@ import com.bigskyway.skyler.prairielandadventures2.util.BaseGameActivity;
 import com.bigskyway.skyler.prairielandadventures2.util.BaseGameUtils;
 import com.bigskyway.skyler.prairielandadventures2.util.GameHelper;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.games.Games;
 import com.google.android.gms.plus.Plus;
@@ -41,13 +46,19 @@ public class MultiplayerMenuActivity extends FragmentActivity
     private static final int RC_UNUSED = 5001;
     private static final int RC_SIGN_IN = 9001;
 
+    // Request code to use when launching the resolution activity
+    private static final int REQUEST_RESOLVE_ERROR = 1001;
+    // Unique tag for the error dialog fragment
+    private static final String DIALOG_ERROR = "dialog_error";
+    // Bool to track whether the app is already resolving an error
+    private boolean mResolvingError = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_multiplayer_main_screen);
-
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
@@ -57,7 +68,6 @@ public class MultiplayerMenuActivity extends FragmentActivity
         EnableGooglePlayFragment pbf = (EnableGooglePlayFragment) getFragmentManager().findFragmentById(R.id.fragmentGooglePlaySwitch);
         pbf.setListener(this);
 
-        super.onCreate(savedInstanceState);
 }
 
     public void launchUnit21FirstChallenge (View view) {
@@ -112,22 +122,22 @@ public class MultiplayerMenuActivity extends FragmentActivity
     }
 
     @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-
-        Log.d(TAG, "onConnectionFailed(): attempting to resolve");
+    public void onConnectionFailed(ConnectionResult result) {
         if (mResolvingConnectionFailure) {
-            Log.d(TAG, "onConnectionFailed(): already resolving");
+            // Already attempting to resolve an error.
             return;
-        }
-
-        if (mSignInClicked || mAutoStartSignInFlow) {
-            mAutoStartSignInFlow = false;
-            mSignInClicked = false;
-            mResolvingConnectionFailure = true;
-            if (!BaseGameUtils.resolveConnectionFailure(this, mGoogleApiClient, connectionResult,
-                    RC_SIGN_IN, getString(R.string.signin_other_error))) {
-                mResolvingConnectionFailure = false;
+        } else if (result.hasResolution()) {
+            try {
+                mResolvingConnectionFailure = true;
+                result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
+            } catch (IntentSender.SendIntentException e) {
+                // There was an error with the resolution intent. Try again.
+                mGoogleApiClient.connect();
             }
+        } else {
+            // Show dialog using GoogleApiAvailability.getErrorDialog()
+            showErrorDialog(result.getErrorCode());
+            mResolvingConnectionFailure = true;
         }
     }
 
@@ -150,22 +160,15 @@ public class MultiplayerMenuActivity extends FragmentActivity
 
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode,
-                                    Intent intent) {
-
-
-        if (requestCode == RC_SIGN_IN) {
-            mSignInClicked = false;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_RESOLVE_ERROR) {
             mResolvingConnectionFailure = false;
             if (resultCode == RESULT_OK) {
-                mGoogleApiClient.connect();
-            } else {
-                // Bring up an error dialog to alert the user that sign-in
-                // failed. The R.string.signin_failure should reference an error
-                // string in your strings.xml file that tells the user they
-                // could not be signed in, such as "Unable to sign in."
-                BaseGameUtils.showActivityResultError(this,
-                        requestCode, resultCode, R.string.signin_failure);
+                // Make sure the app is not already connected or attempting to connect
+                if (!mGoogleApiClient.isConnecting() &&
+                        !mGoogleApiClient.isConnected()) {
+                    mGoogleApiClient.connect();
+                }
             }
         }
     }
@@ -179,4 +182,54 @@ public class MultiplayerMenuActivity extends FragmentActivity
     public void onSignInSucceeded() {
         Log.i(TAG, "Sign in succeeded");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!mResolvingConnectionFailure) {  // more about this later
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    /* Creates a dialog for an error message */
+    private void showErrorDialog(int errorCode) {
+        // Create a fragment for the error dialog
+        ErrorDialogFragment dialogFragment = new ErrorDialogFragment();
+        // Pass the error that should be displayed
+        Bundle args = new Bundle();
+        args.putInt(DIALOG_ERROR, errorCode);
+        dialogFragment.setArguments(args);
+        dialogFragment.show(getFragmentManager(), "errordialog");
+    }
+
+    /* Called from ErrorDialogFragment when the dialog is dismissed. */
+    public void onDialogDismissed() {
+        mResolvingError = false;
+    }
+
+    /* A fragment to display an error dialog */
+    public static class ErrorDialogFragment extends DialogFragment {
+        public ErrorDialogFragment() { }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Get the error code and retrieve the appropriate dialog
+            int errorCode = this.getArguments().getInt(DIALOG_ERROR);
+            return GoogleApiAvailability.getInstance().getErrorDialog(
+                    this.getActivity(), errorCode, REQUEST_RESOLVE_ERROR);
+        }
+
+        @Override
+        public void onDismiss(DialogInterface dialog) {
+            ((MultiplayerMenuActivity) getActivity()).onDialogDismissed();
+        }
+    }
+
+
 }
